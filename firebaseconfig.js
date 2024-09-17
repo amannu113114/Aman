814e9d8dd7bd1d9f9ca692351d43de39ad4088df
Rasnux <rasnux@localhost.localdomain>
2024-09-17 19:57:25 +0530

    firebase

---
+++ firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsc2AtZLoeySdkQ_15qn_krYInyIBwAMg",
+  authDomain: "portfolio-unfinished.firebaseapp.com",
+  projectId: "portfolio-unfinished",
+  storageBucket: "portfolio-unfinished.appspot.com",
+  messagingSenderId: "88917049645",
+  appId: "1:88917049645:web:9d8c937291bacd4c0b53a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
