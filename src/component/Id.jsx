da19ad9dc1c48269f4e029215c6fdf8f4297f281
Rasnux <rasnux@localhost.localdomain>
2024-09-23 21:23:15 +0530

    id card

--- src/component/Id.jsx
+++ src/component/Id.jsx
@@ -2,37 +2,56 @@ import React from 'react'
 import './Id.css'
 
 function Id() {
-  return (
-    <div id='Ids'>
-        <div id="identity">
-            <h3>I</h3>
-            <h3>D</h3>
-            <h3>E</h3>
-            <h3>N</h3>
-            <h3>T</h3>
-            <h3>I</h3>
-            <h3>T</h3>
-            <h3>Y</h3>
-        </div>
-        <div id="identitycard">
-            <div id="name">Aman Kumar</div>
-            <div id="detailed">
-                <div id="detailedinfo">
-                    <p>Gender: Male</p>
-                    <p>Nationality: Indian</p>
-                    <p>Status: Single</p>
-                    <p>D.O.B: 30 March 2005</p>
-                    <p>Address: Makhdumpur</p>
-                    <p>District: Jehanabad</p>
-                    <p>State: Bihar</p>
-                </div>
-                <div id="detailphoto">
+    return (
+        <div id='Ids'>
+            <details>
+                <summary>
+                    <div id="identity">
+                        <h3>I</h3>
+                        <h3>D</h3>
+                        <h3>E</h3>
+                        <h3>N</h3>
+                        <h3>T</h3>
+                        <h3>I</h3>
+                        <h3>T</h3>
+                        <h3>Y</h3>
+                    </div>
+                </summary>
+                <div id="Id">
+                    <div id="boundary">
+                        <div class="id-card">
+                            <div class="photo">
+                                <img src="/aman.jpeg" alt="User Photo" />
+                            </div>
+                            <div class="info">
+                                <div class="company">
+                                    <p>U</p>
+                                    <p>N</p>
+                                    <p>F</p>
+                                    <p>I</p>
+                                    <p>N</p>
+                                    <p>I</p>
+                                    <p>S</p>
+                                    <p>H</p>
+                                    <p>E</p>
+                                    <p>D</p>
+                                </div>
+                                <div class="name">aman kumar</div>
+                                <div id="information">
+                                    <div id="degree"><span>Degree:</span> <br />B. Tech</div>
+                                    <div id="college"><span>College:</span> <br />D.C.E. Darbhanga</div>
+                                    <div id="address"><span>Address:</span><br /> Darbhanga</div>
 
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
+
+            </details>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Id
